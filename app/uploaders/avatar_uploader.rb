cd784ba4e4f7c1ae60ce3c745d72e4fdf095b838
masafumi.tokuhisa <tokuhisa.st@gmail.com>
2019-09-19 14:20:49 +0000

    uploader test

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,22 +1,16 @@
-class ImageUploader < CarrierWave::Uploader::Base
+class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  
-  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
-
- #「storage :file」　が有効になっているとCloudinaryに画像アップロードできない　↓
- 
-  #storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -50,19 +44,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  
-  process :convert => 'png'
-  process :tags => ['image']
-
-  version :standard do
-    process :resize_to_fill => [100, 150, :north]
-  end
-
-  version :thumbnail do
-    process :resize_to_fit => [50, 50]
-  end
-  
-  def public_id
-    return model.id
-  end
 end
