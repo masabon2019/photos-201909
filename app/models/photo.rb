2262dc42d1fd84a4e82da2d8256f4a9aec8033ac
masafumi.tokuhisa <tokuhisa1972@gmail.com>
2020-05-11 17:02:24 +0900

    [update] フォーム修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,18 +1,18 @@
 class Photo < ApplicationRecord
   belongs_to :user
-  
+
   validates :image, presence: true
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: { maximum: 100 }
   validates :day, length: { maximum: 20 }
   validates :equipment, length: { maximum: 50 }
   validates :comment, length: { maximum: 255 }
 
   mount_uploader :image, ImageUploader
-  
-  
+
+
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_users, through: :bookmarks, source: :user
-  
+
   has_many :goods, dependent: :destroy
   has_many :good_users, through: :goods, source: :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 80 },
                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                   uniqueness: { case_sensitive: false }
   validates :prof_image, presence: true
@@ -11,18 +11,18 @@ class User < ApplicationRecord
   validates :url, allow_blank: true, length: { maximum: 255 }, format: /\A#{URI::regexp(%w(http https))}\z/, on: :update
   validates :self_introduction, length: { maximum: 255 }
   has_secure_password
-  
-  mount_uploader :prof_image, ImageUploader 
-  
+
+  mount_uploader :prof_image, ImageUploader
+
   has_many :photos
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-  
+
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_photos, through: :bookmarks, source: :photo
-  
+
   has_many :goods, dependent: :destroy
   has_many :good_photos, through: :goods, source: :photo
 
@@ -39,34 +39,34 @@ class User < ApplicationRecord
 
   def following?(other_user)
   	self.followings.include?(other_user)
-  end 
-  
+  end
+
   def bookmark(other_photo)
     self.bookmarks.find_or_create_by(photo_id: other_photo[:id])
   end
-  
+
   def unbookmark(other_photo)
     bookmark = self.bookmarks.find_by(photo_id: other_photo[:id])
     bookmark.destroy if bookmark
   end
-  
+
   def bookmarking?(other_photo)
     self.bookmark_photos.include?(other_photo)
   end
-  
+
   def good(other_photo)
     self.goods.find_or_create_by(photo_id: other_photo[:id])
   end
-  
+
   def ungood(other_photo)
     good = self.goods.find_by(photo_id: other_photo[:id])
     good.destroy if good
   end
-  
+
   def goodpress?(other_photo)
     self.good_photos.include?(other_photo)
   end
-  
+
   def correct_url?
     self.url != nil
   end
