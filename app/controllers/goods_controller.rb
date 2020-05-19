2f3735ddd57648c3644144408181a0942d157b96
masafumi.tokuhisa <tokuhisa1972@gmail.com>
2020-05-19 11:08:28 +0900

    [update] いいねボタンのAjax化

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,18 +2,20 @@ class GoodsController < ApplicationController
   before_action :require_user_logged_in
 
   def create
-    photo = Photo.find(params[:photo_id])
-    current_user.good(photo)
+    @photo = Photo.find(params[:photo_id])
+    current_user.good(@photo)
     #flash[:success] = '「いいね」しました。'
-    redirect_back(fallback_location: @current_user)
+    #redirect_back(fallback_location: @current_user)
     #render 'index.js.erb'
   end
 
   def destroy
-    photo = Photo.find(params[:photo_id])
-    current_user.ungood(photo)
+    @photo = Photo.find(params[:photo_id])
+    current_user.ungood(@photo)
     #flash[:success] = '「いいね」を解除しました。'
-    redirect_back(fallback_location: @current_user)
-    #render 'index.js.erb'
+    #redirect_back(fallback_location: @current_user)
+
+    #destroyの場合なぜ指定しなければいけないのか？？
+    render 'create.js.erb'
   end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       # form_with 用　インスタンス
-      @photo = current_user.photos.build 
-      
+      @photo = current_user.photos.build
+
       #photos　ログインユーザーの投稿写真一覧
       #@photos = current_user.photos.order(id: :desc).page(params[:page])
       @photos = Photo.all.order(id: :desc).page(params[:page])
