0a59484c932a7fe555f96c125f158550eac06950
masafumi.tokuhisa <tokuhisa1972@gmail.com>
2020-06-14 14:41:04 +0900

    [update] ゲストの情報更新可能（名前、メール、パスワード以外

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,6 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    if (@user.id == 18)
-    flash.now[:danger] = 'ゲストユーザーはユーザー登録情報の変更はできません'
-    end
   end
 
   def update
