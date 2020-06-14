3ee095fb598cad550af4e16a77414c885a45057f
masafumi.tokuhisa <tokuhisa1972@gmail.com>
2020-06-14 13:30:09 +0900

    [add] フォロワーボタンAjax化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,22 @@
 class RelationshipsController < ApplicationController
   before_action :require_user_logged_in
-  
+
   def create
-    user = User.find(params[:follow_id])
-    current_user.follow(user)
-    flash[:success] = 'ユーザーをフォローしました。'
+    @user = User.find(params[:follow_id])
+    current_user.follow(@user)
+    #flash[:success] = 'ユーザーをフォローしました。'
     #redirect_to user
-    redirect_back(fallback_location: user)
+
+    #redirect_back(fallback_location: user)
+    render 'create.js.erb'
   end
 
   def destroy
-    user = User.find(params[:follow_id])
-    current_user.unfollow(user)
-    flash[:success] = 'ユーザーのフォローを解除しました。'
+    @user = User.find(params[:follow_id])
+    current_user.unfollow(@user)
+    #flash[:success] = 'ユーザーのフォローを解除しました。'
     #redirect_to user
-    redirect_back(fallback_location: user)
+    #redirect_back(fallback_location: user)
+    render 'create.js.erb'
   end
 end
